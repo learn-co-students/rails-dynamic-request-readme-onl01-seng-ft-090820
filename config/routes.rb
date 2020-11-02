1f7928ec26864f017eb8643d9661321729f56e20
doosemavis <moosedavis2011@gmail.com>
2020-11-02 12:31:43 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+# Dynamice Routing Example
+
+  # get 'posts/:id', to: 'posts#show'
+
+
+# Resource Routing Example
+
+  resources :posts, only: :show
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
